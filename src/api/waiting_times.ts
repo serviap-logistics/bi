717b15f7526d018eab60f16b3c0b7e875021e9f5
carrichi <roberto@carrichi.com>
2024-09-19 13:59:34 -0600

    fix(heacount): :bug: Actualizacion de tipos respecto a la actualizacion del conteo de overtime

--- src/api/waiting_times.ts
+++ src/api/waiting_times.ts
@@ -16,7 +16,7 @@ export type waiting_time = {
   start_date: string;
   end_date: string;
   week: string;
-  week_hours_accumulated;
+  week_hours: number;
 
   regular_hour_cost: number;
   regular_hours: number;
@@ -28,6 +28,7 @@ export type waiting_time = {
 
   total_cost: number;
   total_hours: number;
+  subtotal: number;
 };
 
 export async function getWaitingTimes(
